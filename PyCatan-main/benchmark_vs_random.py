0a5101553b270f634f1c307dbf8131253db2f5ee
HaoXu <yaekitsune2207@gmail.com>
2025-06-06 20:53:19 +0200

    Copiado de otros

--- PyCatan-main/benchmark_vs_random.py
+++ PyCatan-main/benchmark_vs_random.py
@@ -6,7 +6,7 @@ import csv
 from Agents.RandomAgent import RandomAgent as ra
 from Managers.GameDirector import GameDirector
 
-n_matches = 100
+n_matches = 10
 porcentaje_workers = 0.95
 
 agentes_alumnos = [
